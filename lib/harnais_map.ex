59d1b5a487b1b47b14f2b837e3260faa34105ff2
Ian Rumford <ian@taniwha>
2018-01-15 11:14:36 +0000

    harnais_map: The Map Harness in the Harnais Family

---
+++ lib/harnais_map.ex
@@ -0,0 +1,18 @@
+defmodule HarnaisMap do
+  @moduledoc """
+  Documentation for HarnaisMap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HarnaisMap.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/harnais_map_test.exs
@@ -0,0 +1,8 @@
+defmodule HarnaisMapTest do
+  use ExUnit.Case
+  doctest HarnaisMap
+
+  test "greets the world" do
+    assert HarnaisMap.hello() == :world
+  end
+end
